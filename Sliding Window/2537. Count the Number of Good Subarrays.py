5dbb93ac75e036ec60e8fdd814c04291e0b89542
Rajashekar H S <83490512+RAGNARAOK@users.noreply.github.com>
2025-04-16 16:28:35 +0530

    Arrays

--- Sliding Window/2537. Count the Number of Good Subarrays.py
+++ Sliding Window/2537. Count the Number of Good Subarrays.py
@@ -1,4 +1,4 @@
-# Problem Link :
+# Problem Link : https://leetcode.com/problems/count-the-number-of-good-subarrays/description/?envType=daily-question&envId=2025-04-16
 # Given an integer array nums and an integer k, return the number of good subarrays of nums.
 
 # A subarray arr is good if there are at least k pairs of indices (i, j) such that i < j and arr[i] == arr[j].
